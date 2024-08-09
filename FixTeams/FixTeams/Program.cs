03e84eacd25d17cafb89154675c9d01fe672ae25
frichard0 <frichard0@solutec.fr>
2024-08-09 14:36:00 +0200

    Added source code

---
+++ FixTeams/FixTeams/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace FixTeams
+{
+    class Program
+    {
+        const UInt32 KEYEVENTF_EXTENDEDKEY = 0x0001;
+        const UInt32 KEYEVENTF_KEYUP = 0x0002;
+        const int VK_F15 = 0x7E;
+        const int VK_NUMPAD0 = 0x60;
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                keybd_event((byte)VK_F15, 0, KEYEVENTF_EXTENDEDKEY | 0, 0);
+                keybd_event((byte)VK_F15, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, 0);
+                Console.WriteLine("[" + DateTime.Now.ToString("hh:mm:ss") + "] Click !");
+
+                Thread.Sleep(15000);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FixTeams
+
+## About The Project
+
+This project is for educational purposes and allows you to stay active on Teams Desktop by simulating a regular press on the F15 key which does not exist on our keyboards and which does not trigger any action.
